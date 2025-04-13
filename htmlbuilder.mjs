d440a329edd9a4e3d44f83f8fba1051198ef735d
root <root@mail.julensportofolio.com>
2025-04-13 19:19:24 +0000

    Cambiado para que ahora genere de golpe todos los ficheros para cada "lang" dentro de lang.js

--- htmlbuilder.mjs
+++ htmlbuilder.mjs
@@ -1,41 +1,41 @@
 #! /root/.nvm/versions/node/v21.4.0/bin/node
 
-/**
-* Errores conocidos que me da pereza corregir aunque sean una tonteria:
-* 1. Añade al fichero seleccionado toda la traducción, es decir, si ejecutas la script 2 veces sobre el mismo fichero
-* hará append a todo
-* 2. No se puede usar la ñ  :( muy eennne por mi parte
-*
-*/
+
 import fs from 'node:fs'
 import process from 'node:process'
 import * as Langfile from './lang.js'
 
 
 const fileName = process.argv[2];
-const lang = process.argv[3];
-const newFileName = get_lang_filename()
-
 const htmlFile = fs.readFileSync("./" + fileName , "UTF-8")
-const newFile = fs.openSync( newFileName, "a+")
-const htmlLines = htmlFile.split("\n")
-
-htmlLines.forEach((line,n) => {
+let lang = ""
 
 
- const reg = new RegExp(/(?<=ñ)[a-zA-Z\.0-9]+(?=ñ)/gm)
- const matches =  [...line.matchAll(reg)]
+for (const _lang in Langfile.default.languages ){
+	lang = _lang
+	const newFileName = get_lang_filename()
+	fs.unlinkSync(newFileName)
+
+	const newFile = fs.openSync( newFileName, "ax")
+	const htmlLines = htmlFile.split("\n")
+
+	htmlLines.forEach((line,n) => {
+
+
+		 const reg = new RegExp(/(?<=ñ)[a-zA-Z\.0-9]+(?=ñ)/gm)
+		 const matches =  [...line.matchAll(reg)]
  
- for(const match of matches){
-	const m = match[0]
-	line = replacer(m, line)
- }
+		 for(const match of matches){
+			const m = match[0]
+			line = replacer(m, line)
+		}
 
  
- fs.writeSync(newFile, line + '\n'); 
+		 fs.writeSync(newFile, line + '\n'); 
 
 
-})
+	})
+}
 
 function get_lang_filename()
 {
